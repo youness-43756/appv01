f36e821db2f151d508ab48f02f08967a46ef64bc
youness <adxus405@gmail.com>
2024-01-17 18:33:19 +0100

    styling button

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
+    <div className="w-screen h-screen grid content-center place-content-center">
       <Link href="/magic-supp-resis">
         <button className="btn btn-info">Start</button>
       </Link>
